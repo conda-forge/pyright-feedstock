6833ed6369bcdbd9ee099b2af30f2fe9385c00a0
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-06 16:54:18 +0000

    comment

--- recipe/build_node.bat
+++ recipe/build_node.bat
@@ -2,6 +2,7 @@ mkdir %PREFIX%\lib
 mkdir %PREFIX%\lib\pyright
 
 @REM errors if we don't &&
+@REM see https://github.com/prefix-dev/rattler-build/issues/1165
 pnpm config set symlink false  && ^
 pnpm install pyright@%PKG_VERSION% --prefix=%PREFIX%\lib\pyright && ^
 pushd %PREFIX%\lib\pyright && ^
