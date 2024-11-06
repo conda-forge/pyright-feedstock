300eb0f11330456150869167b592cb8c332bef31
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-06 16:53:27 +0000

    fix

--- recipe/build_python.bat
+++ recipe/build_python.bat
@@ -4,7 +4,7 @@ REM Install Python dependencies
 %PYTHON% -m pip install .
 
 REM Create activation script directory and set the Pyright cache location
-mkdir %PREFIX%\etc\conda\activate.d || goto :error
+mkdir %PREFIX%\etc\conda\activate.d
 echo "set PYRIGHT_PYTHON_CACHE_DIR=%PREFIX%\lib\pyright" > %PREFIX%\etc\conda\activate.d\10-set-npm-pyright-location.bat || goto :error
 
 goto :EOF
