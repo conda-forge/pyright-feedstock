f39f49462c9e3b35326affdc0c9ca94273f19c0f
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-06 16:47:09 +0000

    wip

--- recipe/build_node.bat
+++ recipe/build_node.bat
@@ -1,10 +1,11 @@
-@echo on
-
+mkdir %PREFIX%\lib
 mkdir %PREFIX%\lib\pyright
-pnpm install pyright@%PKG_VERSION% --prefix=%PREFIX%\lib\pyright || goto :error
 
-pushd %PREFIX%\lib\pyright && pnpm-licenses generate-disclaimer --prod --json-input --output-file=%SRC_DIR%\third-party-licenses.txt || goto :error
-
+@REM errors if we don't &&
+pnpm config set symlink false  && ^
+pnpm install pyright@%PKG_VERSION% --prefix=%PREFIX%\lib\pyright && ^
+pushd %PREFIX%\lib\pyright && ^
+pnpm-licenses generate-disclaimer --prod --output-file=%SRC_DIR%\third-party-licenses.txt && ^
 popd
 
 goto :EOF
