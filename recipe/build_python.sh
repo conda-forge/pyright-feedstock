d02f2c1d3a4ba028d3170f0cc3e3bb2300ae1c66
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-11 10:20:44 +0000

    cleanup

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -6,11 +6,3 @@ export PYRIGHT_PYTHON_CACHE_DIR="$PREFIX/lib/pyright"
 
 mkdir -p $PREFIX/etc/conda/activate.d
 echo 'export PYRIGHT_PYTHON_CACHE_DIR="$PREFIX/lib/pyright"' > $PREFIX/etc/conda/activate.d/10-set-npm-pyright-location.sh
-
-# pushd $PYRIGHT_PYTHON_CACHE_DIR/pyright-python/$PKG_VERSION/node_modules/pyright/
-
-# generate third party licenses file
-# pnpm install
-# pnpm licenses list --json | pnpm-licenses generate-disclaimer --prod --json-input --output-file=$SRC_DIR/third-party-licenses.txt
-
-# popd
