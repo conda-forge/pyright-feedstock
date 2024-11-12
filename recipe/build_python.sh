934032592ec53902d75f64f374a04002e34acc0d
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-12 20:50:29 +0000

    nomore noarch, fix cache dir location

--- recipe/build_python.sh
+++ recipe/build_python.sh
@@ -5,4 +5,4 @@ $PYTHON -m pip install .
 export PYRIGHT_PYTHON_CACHE_DIR="$PREFIX/lib/pyright"
 
 mkdir -p $PREFIX/etc/conda/activate.d
-echo 'export PYRIGHT_PYTHON_CACHE_DIR="$PREFIX/lib/pyright"' > $PREFIX/etc/conda/activate.d/10-set-npm-pyright-location.sh
+echo 'export PYRIGHT_PYTHON_CACHE_DIR="$CONDA_PREFIX/lib/pyright/"' > $PREFIX/etc/conda/activate.d/10-set-npm-pyright-location.sh
